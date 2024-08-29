a877c05bc4033613b0a492a537b87213569c4028
Andrew <andrew@alsmith.net>
2024-08-28 20:56:01 -0500

    working on font sizes and reuse

--- string/string.h
+++ string/string.h
@@ -298,6 +298,27 @@ zstr_eq(char* str0, char* str1)
     return (*cursor0 == '\0' && *cursor1 == '\0');
 }
 
+function u32
+string_hash(String const& str)
+{
+    u32 result = StartHash(str.data, str.length);
+    return result;
+}
+
+function u32
+string_hash_lowercase(String const& str)
+{
+    u32 result = StartHash();
+
+    // @Slow
+    for (int i = 0; i < str.length; i++)
+    {
+        result = BuildHash((void*)(str.data + i), 1, result);
+    }
+
+    return result;
+}
+
 function bool
 string_eq(String str0, char* str1)
 {
