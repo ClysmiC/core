79d1a58789f8cd23e17f7da3ac25e13530383ede
Andrew <andrew@alsmith.net>
2026-01-18 15:44:46 -0600

    untemplatizing fix32

--- math/fixed_math.h
+++ math/fixed_math.h
@@ -1,45 +1,31 @@
 namespace fxp
 {
-// TODO - consteval gives best compile-time determinism guarantee for converting float values to fixed values, but requires C++20.
-#if (__cplusplus >= 202002L)
- #define FXPCONSTEVAL consteval
-#else
- #define FXPCONSTEVAL constexpr
-#endif
 
-template<class> struct integer_type { static bool constexpr is_supported = false; };
-template<> struct integer_type<i32> { static bool constexpr is_supported = true; using unsigned_t = u32; };
-template<> struct integer_type<i64> { static bool constexpr is_supported = true; using unsigned_t = u64; };
-
-using denom_t = i32;
-
-template<class T, denom_t DBITS>
-struct Value
+struct fix32
 {
-    STATIC_ASSERT(DBITS >= 0);
-    STATIC_ASSERT(integer_type<T>::is_supported);
-
-    static denom_t constexpr D_BITS = DBITS;
-    static denom_t constexpr D = (1 << DBITS);
-    T n;
+    static i32 constexpr DBITS = 10;
+    static i32 constexpr D = (1 << DBITS);
+    i32 n;
 
     // --- Implicitly convert from...
 
-    constexpr Value() = default;
+    constexpr fix32() = default;
+    constexpr fix32(i8 v)   : n((i32)(v * D)) {}
+    constexpr fix32(i16 v)  : n((i32)(v * D)) {}
+    constexpr fix32(i32 v)  : n((i32)(v * D)) {}
+    constexpr fix32(i64 v)  : n((i32)(v * D)) {}
+    constexpr fix32(u8 v)   : n((i32)(v * D)) {}
+    constexpr fix32(u16 v)  : n((i32)(v * D)) {}
+    constexpr fix32(u32 v)  : n((i32)(v * D)) {}
+    constexpr fix32(u64 v)  : n((i32)(v * D)) {}
 
-    template <class T_OTHER>
-    constexpr Value(Value<T_OTHER, DBITS> const& v) : n(T(v.n)) {}
-    constexpr Value(i8 v)   : n(T(v * D)) {}
-    constexpr Value(i16 v)  : n(T(v * D)) {}
-    constexpr Value(i32 v)  : n(T(v * D)) {}
-    constexpr Value(i64 v)  : n(T(v * D)) {}
-    constexpr Value(u8 v)   : n(T(v * D)) {}
-    constexpr Value(u16 v)  : n(T(v * D)) {}
-    constexpr Value(u32 v)  : n(T(v * D)) {}
-    constexpr Value(u64 v)  : n(T(v * D)) {}
-
-    FXPCONSTEVAL Value(f32 v)   : n(T(v * D)) {}
-    FXPCONSTEVAL Value(f64 v)   : n(T(v * D)) {}
+#if (__cplusplus >= 202002L)
+    consteval fix32(f32 v)   : n((i32)(v * D)) {}
+    consteval fix32(f64 v)   : n((i32)(v * D)) {}
+#else
+    constexpr fix32(f32 v)   : n((i32)(v * D)) {}
+    constexpr fix32(f64 v)   : n((i32)(v * D)) {}
+#endif
 
     // --- Explicitly convert to...
 
@@ -55,171 +41,174 @@ struct Value
     explicit constexpr operator f64() const { return f64(n / (f64)D); }
 };
 
+struct fix64
+{
+    static i32 constexpr DBITS = 2 * fix32::DBITS;
+    static i32 constexpr D = (1 << DBITS);
+    i64 n;
+
+    // --- Implicitly convert from...
+
+    constexpr fix64() = default;
+    constexpr fix64(fix32 const& v) : n((i64)(v.n * (1 << (DBITS - fix32::DBITS)))) {}
+    constexpr fix64(i8 v)   : n((i64)(v * D)) {}
+    constexpr fix64(i16 v)  : n((i64)(v * D)) {}
+    constexpr fix64(i32 v)  : n((i64)(v * D)) {}
+    constexpr fix64(i64 v)  : n((i64)(v * D)) {}
+    constexpr fix64(u8 v)   : n((i64)(v * D)) {}
+    constexpr fix64(u16 v)  : n((i64)(v * D)) {}
+    constexpr fix64(u32 v)  : n((i64)(v * D)) {}
+    constexpr fix64(u64 v)  : n((i64)(v * D)) {}
+
+#if (__cplusplus >= 202002L)
+    consteval fix64(f32 v)   : n((i64)(v * D)) {}
+    consteval fix64(f64 v)   : n((i64)(v * D)) {}
+#else
+    constexpr fix64(f32 v)   : n((i64)(v * D)) {}
+    constexpr fix64(f64 v)   : n((i64)(v * D)) {}
+#endif
+
+    // --- Explicitly convert to...
+    explicit constexpr operator i8()  const { return i8 (n / D); }
+    explicit constexpr operator i16() const { return i16(n / D); }
+    explicit constexpr operator i32() const { return i32(n / D); }
+    explicit constexpr operator i64() const { return i64(n / D); }
+    explicit constexpr operator u8()  const { return u8 (n / D); }
+    explicit constexpr operator u16() const { return u16(n / D); }
+    explicit constexpr operator u32() const { return u32(n / D); }
+    explicit constexpr operator u64() const { return u64(n / D); }
+    explicit constexpr operator f32() const { return f32(n / (f32)D); }
+    explicit constexpr operator f64() const { return f64(n / (f64)D); }
+    explicit constexpr operator fix32() const
+    {
+        fix32 result;
+        result.n = (i32)(n / (1 << (DBITS - fix32::DBITS)));
+        return result;
+    }
+};
+
 // --- Math operations
 
-template<class T, denom_t DBITS>
-Value<T, DBITS> constexpr
-operator+(
-    Value<T, DBITS> v0,
-    Value<T, DBITS> v1)
+function fix32 constexpr
+operator+(fix32 v0, fix32 v1)
 {
-    Value<T, DBITS> result;
+    fix32 result;
     result.n = v0.n + v1.n;
     return result;
 }
 
-template<class T, denom_t DBITS>
-Value<T, DBITS> constexpr&
-operator+=(
-    Value<T, DBITS>& v0,
-    Value<T, DBITS> v1)
+function fix32 constexpr&
+operator+=(fix32& v0, fix32 v1)
 {
     v0 = v0 + v1;
     return v0;
 }
 
-template<class T, denom_t DBITS>
-Value<T, DBITS> constexpr
-operator-(
-    Value<T, DBITS> v0,
-    Value<T, DBITS> v1)
+function fix32 constexpr
+operator-(fix32 v0, fix32 v1)
 {
-    Value<T, DBITS> result;
+    fix32 result;
     result.n = v0.n - v1.n;
     return result;
 }
 
-template<class T, denom_t DBITS>
-Value<T, DBITS> constexpr&
-operator-=(
-    Value<T, DBITS>& v0,
-    Value<T, DBITS> v1)
+function fix32 constexpr&
+operator-=(fix32& v0, fix32 v1)
 {
     v0 = v0 - v1;
     return v0;
 }
 
-template<class T, denom_t DBITS>
-Value<T, DBITS> constexpr
-operator*(
-    Value<T, DBITS> v0,
-    Value<T, DBITS> v1)
+function fix32 constexpr
+operator*(fix32 v0, fix32 v1)
 {
-    Value<T, DBITS> result;
+    fix32 result;
     i64 n = (i64)v0.n * (i64)v1.n;
-    n /= (1 << DBITS);
-    result.n = (T)n;
+    n /= (1 << fix32::DBITS);
+    result.n = (i32)n;
     return result;
 }
 
-template<class T, denom_t DBITS>
-Value<T, DBITS> constexpr&
-operator*=(
-    Value<T, DBITS>& v0,
-    Value<T, DBITS> v1)
+function fix32 constexpr&
+operator*=(fix32& v0, fix32 v1)
 {
     v0 = v0 * v1;
     return v0;
 }
 
-template<class T, denom_t DBITS>
-Value<T, DBITS> constexpr
-operator/(
-    Value<T, DBITS> v0,
-    Value<T, DBITS> v1)
+function fix32 constexpr
+operator/(fix32 v0, fix32 v1)
 {
-    Value<T, DBITS> result;
-    result.n = (T)(((1 << DBITS) * (i64)v0.n) / v1.n);
+    fix32 result;
+    result.n = (i32)(((1 << fix32::DBITS) * (i64)v0.n) / v1.n);
     return result;
 }
 
-template<class T, denom_t DBITS>
-Value<T, DBITS> constexpr&
-operator/=(
-    Value<T, DBITS>& v0,
-    Value<T, DBITS> v1)
+function fix32 constexpr&
+operator/=(fix32& v0, fix32 v1)
 {
     v0 = v0 / v1;
     return v0;
 }
 
-template<class T, denom_t DBITS>
 bool constexpr
-operator==(
-    Value<T, DBITS> v0,
-    Value<T, DBITS> v1)
+operator==(fix32 v0, fix32 v1)
 {
     bool result = (v0.n == v1.n);
     return result;
 }
 
-template<class T, denom_t DBITS>
 bool constexpr
-operator!=(
-    Value<T, DBITS> v0,
-    Value<T, DBITS> v1)
+operator!=(fix32 v0, fix32 v1)
 {
     bool result = (v0.n != v1.n);
     return result;
 }
 
-template<class T, denom_t DBITS>
 bool constexpr
-operator>(
-    Value<T, DBITS> v0,
-    Value<T, DBITS> v1)
+operator>(fix32 v0, fix32 v1)
 {
     bool result = (v0.n > v1.n);
     return result;
 }
 
-template<class T, denom_t DBITS>
 bool constexpr
-operator>=(
-    Value<T, DBITS> v0,
-    Value<T, DBITS> v1)
+operator>=(fix32 v0, fix32 v1)
 {
     bool result = (v0.n >= v1.n);
     return result;
 }
 
-template<class T, denom_t DBITS>
 bool constexpr
-operator<(
-    Value<T, DBITS> v0,
-    Value<T, DBITS> v1)
+operator<(fix32 v0, fix32 v1)
 {
     bool result = (v0.n < v1.n);
     return result;
 }
 
-template<class T, denom_t DBITS>
 bool constexpr
-operator<=(
-    Value<T, DBITS> v0,
-    Value<T, DBITS> v1)
+operator<=(fix32 v0, fix32 v1)
 {
     bool result = (v0.n <= v1.n);
     return result;
 }
 
 // TODO - delete this function in favor of vec scaling functions
-template<class T, denom_t DBITS>
-Value<T, DBITS>
-sqrt(Value<T, DBITS> v)
+function fix32
+sqrt(fix32 v)
 {
-    Value<T, DBITS> result = {};
+    fix32 result = {};
     if (v.n <= 0)
         return result;
 
     int msb = 0;
-    bitscan_msb_index((integer_type<T>::unsigned_t)v.n, &msb);
+    bitscan_msb_index((u32)v.n, &msb);
 
     int denom_msb = 0;
-    bitscan_msb_index((u32)(1 << DBITS), &denom_msb);
+    bitscan_msb_index((u32)(1 << fix32::DBITS), &denom_msb);
 
     // Initial guess
-    result = T(1) << ((msb - denom_msb) / 2);
+    result = 1 << ((msb - denom_msb) / 2);
     if (result.n <= 0)
     {
         // Avoid divide by 0
@@ -228,19 +217,17 @@ sqrt(Value<T, DBITS> v)
 
     // Empirically, all integer 22.10 values from 0 to (1<<21)-1 converge to within < 0.035% of the actual value after 3 iterations
     int constexpr ITERATION_COUNT = 3;
-    Value<T, DBITS> constexpr TWO(2);
+    fix32 constexpr TWO(2);
 
     // Newton's Method
     for (int i = 0; i < ITERATION_COUNT; i++)
     {
-        Value<T, DBITS> div = v / result;
+        fix32 div = v / result;
         result = (result + div) / TWO;
     }
 
     return result;
 }
-
-#undef FXPCONSTEVAL
 
 } // namespace fxp
 
